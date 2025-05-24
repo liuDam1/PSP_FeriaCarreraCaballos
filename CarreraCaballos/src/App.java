83368d52a972a5eedae5ce2507566be73376c54e
liuDam1 <liu.jiangyu.dam@gmail.com>
2025-05-24 19:18:52 +0200

    Entrar al inicio ok

---
+++ CarreraCaballos/src/App.java
@@ -0,0 +1,19 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    public static final String RUTA_INICIO = "/vista/Inicio.fxml";
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource(RUTA_INICIO));
+        primaryStage.setScene(new Scene(root, 831, 446));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
